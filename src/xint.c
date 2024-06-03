3617b07464dcd191ffd1345ced11eb004e13e266
Gerard Green <gerryg@anvilos.com>
2024-06-03 22:34:31 +1000

    Moved functions from xint_signed to xint

--- src/xint.c
+++ src/xint.c
@@ -14,6 +14,7 @@ static void trim_zeroes(xint_t u);
 static int get_highest_word(xint_t x);
 static int get_highest_bit(xword_t word);
 static int resize(xint_t x, int new_size);
+static int add_signed(xint_t w, const xint_t u, const xint_t v, int Upos, int Vpos);
 
 static void x_zero(xword_t *Y, size_t sz);
 static void x_move(xword_t *Y, xword_t *X, size_t sz);
@@ -138,6 +139,26 @@ void xint_assign_long(xint_t u, long val)
     }
 }
 
+int xint_cmp_ulong(const xint_t u, const unsigned long val)
+{
+    return u->size < 0 ? -1 : xint_cmpa_ulong(u, val);
+}
+
+int xint_cmp_long(const xint_t u, const long val)
+{
+    if (val >= 0)
+    {
+        return xint_cmp_ulong(u, val);
+    }
+    // val is neg
+    if (u->size >= 0)
+    {
+        return 1;
+    }
+    // Both are neg
+    return -xint_cmpa_ulong(u, -val);
+}
+
 int xint_cmpa_ulong(const xint_t u, const unsigned long v)
 {
     if (v <= XWORD_MAX)
@@ -230,6 +251,56 @@ int xint_adda_ulong(xint_t w, const xint_t u, const unsigned long v)
     return xint_adda(w, u, vv);
 }
 
+int xint_add_ulong(xint_t w, const xint_t u, const unsigned long val)
+{
+    return xint_adda_ulong(w, u, val);
+}
+
+int xint_add_long(xint_t w, const xint_t u, const long val)
+{
+    return val >= 0 ? xint_add_ulong(w, u, val) : xint_sub_ulong(w, u, -val);
+}
+
+int xint_add(xint_t w, const xint_t u, const xint_t v)
+{
+    return add_signed(w, u, v, !xint_is_neg(u), !xint_is_neg(v));
+}
+
+int xint_sub_ulong(xint_t w, const xint_t u, const unsigned long val)
+{
+    return xint_suba_ulong(w, u, val);
+}
+
+int xint_sub_long(xint_t w, const xint_t u, const long val)
+{
+    return val >= 0 ? xint_sub_ulong(w, u, val) : xint_add_ulong(w, u, val);
+}
+
+int xint_sub(xint_t w, const xint_t u, const xint_t v)
+{
+    return add_signed(w, u, v, !xint_is_neg(u), xint_is_neg(v));
+}
+
+static int add_signed(xint_t w, const xint_t u, const xint_t v, int upos, int vpos)
+{
+    if (upos == vpos)
+    {
+        xint_adda(w, u, v);
+        upos ? xint_set_pos(w) : xint_set_neg(w);
+    }
+    else
+    {
+        if (xint_suba(w, u, v) < 0)
+        {
+            upos ? xint_set_neg(w) : xint_set_pos(w);
+        }
+        else
+        {
+            upos ? xint_set_pos(w) : xint_set_neg(w);
+        }
+    }
+    return -1;
+}
 int xint_suba(xint_t w, const xint_t u, const xint_t v)
 {
     // XXX: what about if u or v is 0
