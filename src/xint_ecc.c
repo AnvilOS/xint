afef867dec872740d9cf0992360203abcce4cf94
Gerard Green <gerryg@anvilos.com>
2026-01-24 15:59:04 +1100

    Working ECC verify

--- src/xint_ecc.c
+++ src/xint_ecc.c
@@ -805,6 +805,52 @@ int xint_ecc_sign_det(xint_ecc_sig_t sig, unsigned char *digest, int digest_len,
     return 0;
 }
 
+void xint_point_add_p(xint_ecc_point_t r, xint_ecc_point_t q, xint_ecc_point_t p, xint_t m)
+{
+    xint_t diffy = XINT_INIT_VAL;
+    xint_t diffx = XINT_INIT_VAL;
+    xint_t lambda = XINT_INIT_VAL;
+    xint_t xr = XINT_INIT_VAL;
+    xint_t yr = XINT_INIT_VAL;
+    
+    if (p->is_at_infinity)
+    {
+        xint_point_copy(r, q);
+        return;
+    }
+
+    if (q->is_at_infinity)
+    {
+        xint_point_copy(r, p);
+        return;
+    }
+
+    xint_sub(diffy, q->y, p->y);
+    xint_mod(diffy, diffy, m);
+    xint_sub(diffx, q->x, p->x);
+    xint_mod(diffx, diffx, m);
+    xint_mod_inverse(diffx, diffx, m);
+    xint_mul(lambda, diffy, diffx);
+    xint_mod(lambda, lambda, m);
+        
+    xint_sqr(xr, lambda);
+    xint_sub(xr, xr, p->x);
+    xint_mod(xr, xr, m);
+    xint_sub(xr, xr, q->x);
+    xint_mod(xr, xr, m);
+
+    xint_sub(yr, p->x, xr);
+    xint_mod(yr, yr, m);
+    xint_mul(yr, yr, lambda);
+    xint_mod(yr, yr, m);
+    xint_sub(yr, yr, p->y);
+    xint_mod(yr, yr, m);
+
+    xint_mod(r->x, xr, m);
+    xint_mod(r->y, yr, m);
+    r->is_at_infinity = 0;
+}
+
 void xint_point_add(xint_ecc_point_t r, xint_ecc_point_t q, xint_ecc_point_t p, xint_t a, xint_t m)
 {
     xint_t sumy = XINT_INIT_VAL;
@@ -861,16 +907,17 @@ int xint_ecc_verify(xint_ecc_sig_t sig, unsigned char *digest, int digest_len, x
     xint_from_bin(h1_int, digest, digest_len);
     xint_rshift(h1_int, h1_int, xint_size(h1_int) * XWORD_BITS - p256.nbits);
     xint_mod(h1_int, h1_int, N);
-    //xint_print_hex("h1", h1_int);
+    xint_print_hex("h1", h1_int);
 
     // Inverse of s
     xint_t s_inv = XINT_INIT_VAL;
     xint_mod_inverse(s_inv, sig->s, N);
-    //xint_print_hex("s_inv", s_inv);
+    xint_print_hex("s_inv", s_inv);
+    printf("================================================================\n");
 
-    xint_t inv_chk = XINT_INIT_VAL;
-    xint_mul(inv_chk, sig->s, s_inv);
-    xint_mod(inv_chk, inv_chk, N);
+//    xint_t inv_chk = XINT_INIT_VAL;
+//    xint_mul(inv_chk, sig->s, s_inv);
+//    xint_mod(inv_chk, inv_chk, N);
     //xint_print_hex("inv_chk", inv_chk);
 
     // u1 = z * inv_s
@@ -890,24 +937,52 @@ int xint_ecc_verify(xint_ecc_sig_t sig, unsigned char *digest, int digest_len, x
     xint_add(mmm, mmm, h1_int);
     xint_mul(mmm, mmm, s_inv);
     xint_mod(mmm, mmm, N);
-
     xint_ecc_point_t p1;
     xint_point_init(p1);
-
     xint_ecc_mul_scalar(p1, c->Gx, c->Gy, mmm, c);
-
     xint_print_hex("Cx", p1->x);
     xint_print_hex("Cy", p1->y);
+    printf("================================================================\n");
     
+    xint_assign_str(mmm, priv, 0);
+    xint_mul(mmm, mmm, u2);
+    xint_add(mmm, mmm, u1);
+    xint_mod(mmm, mmm, N);
     xint_ecc_point_t p2;
     xint_point_init(p2);
-    char *k = "0xA6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60";
-    xint_t kkk = XINT_INIT_VAL;
-    xint_assign_str(kkk, k, 0);
-    xint_ecc_mul_scalar(p2, c->Gx, c->Gy, kkk, c);
-
+    xint_ecc_mul_scalar(p2, c->Gx, c->Gy, mmm, c);
     xint_print_hex("Cx", p2->x);
     xint_print_hex("Cy", p2->y);
+    printf("================================================================\n");
+
+    xint_ecc_point_t p3;
+    xint_point_init(p3);
+    p1->is_at_infinity = 0;
+    p2->is_at_infinity = 0;
+
+    xint_ecc_mul_scalar(p1, c->Gx, c->Gy, u1, c);
+    xint_assign_str(mmm, priv, 0);
+    xint_mul(mmm, mmm, u2);
+    xint_mod(mmm, mmm, N);
+    xint_ecc_mul_scalar(p2, c->Gx, c->Gy, mmm, c);
+    xint_point_add_p(p3, p2, p1, P);
+    xint_print_hex("Cx", p3->x);
+    xint_print_hex("Cy", p3->y);
+    printf("================================================================\n");
+
+    
+
+
+    
+//    xint_ecc_point_t p2;
+//    xint_point_init(p2);
+//    char *k = "0xA6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60";
+//    xint_t kkk = XINT_INIT_VAL;
+//    xint_assign_str(kkk, k, 0);
+//    xint_ecc_mul_scalar(p2, c->Gx, c->Gy, kkk, c);
+//
+//    xint_print_hex("Cx", p2->x);
+//    xint_print_hex("Cy", p2->y);
 
 
 //    xint_ecc_point_t p2;
@@ -928,8 +1003,8 @@ int xint_ecc_verify(xint_ecc_sig_t sig, unsigned char *digest, int digest_len, x
 //
 //    xint_print_hex(" n", N);
 //    xint_print_hex("x1", p3->x);
-    xint_print_hex(" r", sig->r);
-    xint_print_hex(" s", sig->s);
+//    xint_print_hex(" r", sig->r);
+//    xint_print_hex(" s", sig->s);
 
     return 0;
 }
