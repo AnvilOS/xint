3dc4ceed04aeb71cb1d63f1324b1bc1d1276bb42
Gerard Green <gerryg@anvilos.com>
2026-01-29 08:03:04 +1100

    First NIST vectors in C structs

---
+++ test/nist_vectors.h
@@ -0,0 +1,14 @@
+
+struct key_pair
+{
+    char *d;
+    char *Qx;
+    char *Qy;
+};
+
+extern struct key_pair key_pairs_p_192[];
+extern struct key_pair key_pairs_p_224[];
+extern struct key_pair key_pairs_p_256[];
+extern struct key_pair key_pairs_p_384[];
+extern struct key_pair key_pairs_p_521[];
+
